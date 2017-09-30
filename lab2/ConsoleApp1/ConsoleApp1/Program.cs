1d676eef2775c9078eb43ae81dc70cb4445b4b3a
annahlv <crispinq@yandex.com>
2017-09-30 13:00:30 +0300

    upd

---
+++ lab2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿//Rextester.Program.Main is the entry point for your code. Don't change it.
+//Compiler version 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace LABA2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+          
+            char symbol = 'Z';   // инициализируем переменную symbol буквенным значением Z
+            float f = 15.7F;   // переменная f инициализируется числовым значением 15.7
+            int x = 5, y = 10, z = 12;    // инициализируем несколько переменных одного типа
+            bool b = true;
+            Console.WriteLine(symbol);
+            Console.WriteLine(f);
+            Console.WriteLine(b);
+        }
+    }
+    // Тип, неявно производный от Object
+    class neyawno
+    {
+        public static void Purum(string[] args)
+
+        {
+
+        }
+    }
+    // Тип, явно производный от Object
+    class yawno : System.Object
+    {
+        public static void Param(string[] args)
+
+        {
+
+        }
+    }
+
+}
---
+++ lab2/types/a.cs
@@ -0,0 +1,13 @@
+//Определите переменные всех возможных примитивных типов С# и проинициализируйте их.
+using System;
+namespace init
+{
+    class class1
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет!");
+        }
+    }
+}
